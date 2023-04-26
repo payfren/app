0adf428720584e13de746456432cfd0fedb4d9f5
Matei Tudose <matei@mateitudose.dev>
2023-04-26 20:08:17 +0300

    Change package.json scripts

--- babel.config.js
+++ babel.config.js
@@ -21,7 +21,8 @@ module.exports = function (api) {
             ],
             "react-native-reanimated/plugin",
             ["module:react-native-dotenv", {
-                safe: '.env.template',
+                safe: true,
+                allowUndefined: false,
             }],
             [require.resolve("expo-router/babel")],
         ],
